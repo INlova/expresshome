22471e3f8ddf049bfc03b0a8d7d80833d28d9a73
Michael A Nowiszewski <micski@gmail.com>
2018-01-27 17:48:13 +0000

    refactor app.js

--- Level 4 and 5 - add, drop, refactor routes/app.js
+++ Level 4 and 5 - add, drop, refactor routes/app.js
@@ -1,16 +1,9 @@
 var express = require('express');
 var app = express();
-
 app.use(express.static('public'));
-
-
-
-
 var router = require('./routes/cities');
 app.use('/cities', router);
-
-
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
-  console.log('Running expressHW project on 3000');
+	console.log('Running expressHW project on 3000');
 });
